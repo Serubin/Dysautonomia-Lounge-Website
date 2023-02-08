38d188d38a7a877c820de1c5bde458a83a7457ab
Solomon <Serubin@serubin.net>
2023-02-08 00:38:38 -0500

    Update redirect page

--- pages/join.tsx
+++ pages/join.tsx
@@ -3,9 +3,10 @@ import Router from "next/router";
 import { inviteSlug } from '../data';
 
 const Join: NextPage = (): JSX.Element => {
+  const inviteLink = `https://discord.gg/${inviteSlug}`;
   const isClient = typeof document !== "undefined";
-  isClient && Router.replace(`https://discord.gg/${inviteSlug}`);
-  return <></>;
+  isClient && Router.replace(inviteLink);
+  return <div style={{'backgroundColor': 'rgba(0, 0, 0, 0.8)'}}>Redirecting to <a href={inviteLink} style={{color: 'white'}}>{inviteLink}</a></div>;
 };
 
 export default Join;
